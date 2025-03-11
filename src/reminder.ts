4355c6b920e79856835eeb05688cfeb70b84cf28
Maheshr57 <mahem7080@gmail.com>
2025-03-11 10:39:27 +0530

    Add reminder.ts file with CRUD functions

---
+++ src/reminder.ts
@@ -0,0 +1,108 @@
+type reminder = {
+    id?: string | null;
+    title?: string;
+    description?: string;
+    dueDate?: string;
+    isCompleted?: boolean;
+}
+
+export class reminderDatabase {
+    private remind:  Map<string, reminder> = new Map();
+
+    exists(id: string): boolean{
+        return this.remind.has(id);
+    }
+
+    createReminder(reminder: reminder): string{
+        let id = Math.random().toString(36).substring(2, 11);
+        reminder.id = id;
+        reminder.isCompleted = false;
+        this.remind.set(id, reminder);
+        console.log(`Reminder created successfully with id: ${id}\n`);
+        return id;
+    }
+
+    getReminder(id: string): reminder | null{
+        return this.remind.get(id) || null;
+    }
+
+    getAllReminders(): reminder[]{
+        return Array.from(this.remind.values());
+    }
+
+    removeReminder(id: string): void{
+        if(!this.exists(id)){
+            console.log("\nReminder not found\n");
+            return;
+        }
+        this.remind.delete(id);
+        console.log('\nReminder removed successfully\n');
+    }
+
+    updateReminder(id: string, reminder: reminder): void{
+        if(!this.exists(id)){
+            console.log("\nReminder not found\n");
+            return;
+        }
+        const existingReminder = this.remind.get(id);
+        const newReminder = { ...existingReminder, ...reminder, id };
+        this.remind.set(id, newReminder);
+        console.log('\nReminder updated successfully\n');
+    }
+
+    markReminderAsCompleted(ids: string[] ): void{
+            ids.forEach(id => {if(!this.exists(id)){
+                console.log("\nReminder not found\n");
+                return;
+            }
+            const reminder = this.remind.get(id)!;
+            reminder.isCompleted = true;
+            this.remind.set(id, reminder);
+            console.log('\nReminder marked as completed\n');
+        });
+    }
+
+    getAllRemindersMarkedAsCompleted(): void{
+        const completedReminders = Array.from(this.remind.values()).filter(reminder => reminder.isCompleted);
+        console.log('\nCompleted Reminders:\n');
+        console.log(completedReminders);
+    }
+
+    getAllRemindersNotMarkedAsCompleted(id: string): void{
+        if(!this.exists(id)){
+            console.log("\nReminder not found\n");
+            return;
+        }
+        const reminder = this.remind.get(id)!;
+        reminder.isCompleted = false;
+        this.remind.set(id, reminder);
+        console.log('\nReminder marked as uncompleted\n');
+    }
+
+    getIncompletedReminders(): void{
+        const incompletedReminders = Array.from(this.remind.values()).filter(reminder => !reminder.isCompleted);
+        console.log('\nIncompleted Reminders:\n');
+        console.log(incompletedReminders);
+    }
+
+    getAllRemindersDueByToday(): void {
+        const today =  new Date();
+        today.setHours(0, 0, 0, 0);
+
+        const remindersDueByToday = Array.from(this.remind.values()).filter(reminder => {
+            if (!reminder.dueDate) {
+                return false;
+            }
+            const dueDate = new Date(reminder.dueDate);
+            return dueDate < today;
+        }); 
+
+        console.log('\nReminders due by today:\n');
+        if (remindersDueByToday.length === 0) {
+            console.log('No reminders found\n');
+        }
+        else{
+            console.log(remindersDueByToday);
+        }
+    }
+}
